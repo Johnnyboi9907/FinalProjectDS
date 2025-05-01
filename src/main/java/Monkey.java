d522cf6e7f9fe457383db200e12f2af9bbd11475
johnn <johnnytalexiou@icloud.com>
2025-04-30 21:18:58 -0400

    Added Monkey subclass

---
+++ src/main/java/Monkey.java
@@ -0,0 +1,43 @@
+import java.util.Objects;
+
+public class Monkey extends Animal{
+    private String breed;
+
+    public Monkey(String nickname, Gender gender, int age, String quality, String breed) {
+        super(nickname, gender, age, quality);
+        this.breed = breed;
+    }
+
+    @Override
+    public void makeNoise() {
+        System.out.println("OU OU AH AH AH!!!");
+    }
+
+    @Override
+    public String toString() {
+        return "Monkey{" + super.toString() +
+                "breed='" + breed + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Monkey monkey = (Monkey) o;
+        return Objects.equals(breed, monkey.breed);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), breed);
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+}
